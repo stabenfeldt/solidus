d6e342d90829b7dd3719b69065b0850db6834b41
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-23 10:12:12 +0100

    Renamed permission set to MultiVendor

---
+++ core/lib/spree/permission_sets/multi_vendor.rb
@@ -0,0 +1,32 @@
+module Spree
+  module PermissionSets
+    # A user should only have access to items belonging to the stock locations he belongs to.
+    # He should only see activity belonging to his items.
+    # E.g. Stock managment and orders
+    #
+    # Users can be associated with stock locations via the admin user interface.
+    #
+    # @see Spree::PermissionSets::Base
+
+    class MultiVendor < PermissionSets::Base
+      def activate!
+        if user.stock_locations.any?
+
+          can :admin, Spree::StockItem, source_location_id: user_location_ids + [nil], shipped_at: nil
+          can :display, Spree::StockItem, source_location_id: user_location_ids
+          can :manage, Spree::StockItem, source_location_id: user_location_ids
+        end
+      end
+
+      private
+
+      def user_location_ids
+        @user_location_ids ||= user.stock_locations.pluck(:id)
+      end
+
+      def not_permitted_location_ids
+        @not_permitted_location_ids ||= Spree::StockLocation.where.not(id: user_location_ids).pluck(:id)
+      end
+    end
+  end
+end
