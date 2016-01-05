2b0de3cc8b14d7f34ac7917df0c9e6e9f9f2924b
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-05 14:20:03 +0100

    Started on implementing items_belonging_to_user

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -189,9 +189,13 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
 
     def collection
       return parent.send(controller_name) if parent_data.present?
+      byebug
       if model_class.respond_to?(:accessible_by) && !current_ability.has_block?(params[:action], model_class)
         #model_class.accessible_by(current_ability, action)
         model_class.all
+        if model_class == Spree::Product
+          Spree::Product.items_belonging_to_user(current_user)
+        end
       else
         model_class.where(nil)
       end
