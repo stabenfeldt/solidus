5b32733f5ce6fe42336f0297ded7c504bd5e890b
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-07 22:33:44 +0100

    allow admin to see all products. This logic should be moved into the Ability.

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -191,10 +191,10 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
       return parent.send(controller_name) if parent_data.present?
       if model_class.respond_to?(:accessible_by) && !current_ability.has_block?(params[:action], model_class)
         #model_class.accessible_by(current_ability, action)
-        if model_class == Spree::Product
-          Spree::Product.items_belonging_to_user(try_spree_current_user)
-        else
+        if try_spree_current_user.admin?
           model_class.all
+        elsif model_class == Spree::Product
+          Spree::Product.items_belonging_to_user(try_spree_current_user)
         end
       else
         model_class.where(nil)
