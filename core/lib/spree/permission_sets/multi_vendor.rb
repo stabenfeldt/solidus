e41fa66377fed6143febe66f486eeb8d418a8450
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-01-12 18:21:54 +0100

    allow more

--- core/lib/spree/permission_sets/multi_vendor.rb
+++ core/lib/spree/permission_sets/multi_vendor.rb
@@ -18,19 +18,22 @@ module Spree
         cannot :manage, :all
 
         cannot :all, Spree::StockItem
-        can    :view, Spree::StockItem,
-          :stock_location_id => user.stock_locations.present? && user.stock_locations.first.id
+        can    :view, Spree::StockItem, :stock_location_id => user.stock_locations.present? && user.stock_locations.first.id
 
         cannot :all, %W[
             Spree::Product
         ]
 
-
-        cannot :all, Spree::Product
+        #cannot :all, Spree::Product
         can :index, Spree::Product
-        can [:display, :admin, :edit, :list, :update], Spree::Product do |product|
+        can :create, Spree::Product
+        can [:manage], Spree::Product do |product|
+          product.master.stock_locations.blank? ||
           product.master.stock_locations.first.id == user.stock_locations.first.id
         end
+
+        can [:manage], Spree::Image
+
 
         # p.master.stock_locations.first.id
             #Spree::Image
@@ -47,7 +50,7 @@ module Spree
         #can [:display, :admin], Spree::Variant
         #can [:display, :admin], Spree::OptionValue
         #can [:display, :admin], Spree::ProductProperty
-        #can [:display, :admin], Spree::OptionType
+        # can [:display, :admin, :create], Spree::OptionType
         #can [:display, :admin], Spree::Property
         #can [:display, :admin], Spree::Prototype
         #can [:display, :admin], Spree::Taxonomy
